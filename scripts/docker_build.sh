1ad891489f9b8281211583dc429e39a966031ef2
Jiaxing Shi <quic_jiaxshi@quicinc.com>
2024-11-28 15:52:24 +0800

    Fixed: add Copyright to script
    Signed-off-by: Jiaxing Shi <quic_jiaxshi@quicinc.com>

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+#
 
 export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+#
 
 HOST_FILES=()
 
